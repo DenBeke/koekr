17e75a4c1e1352283a4a71c5146caca9e9aa14fb
MathiasB <git@denbeke.be>
2017-08-26 12:52:20 +0200

    watcher.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/BurntSushi/toml"
-	"github.com/howeyc/fsnotify"
 	log "github.com/sirupsen/logrus"
 	"html/template"
 	"io/ioutil"
@@ -143,75 +142,6 @@ func main() {
 
 	k.GenerateAllPages()
 
-	// Watch for changes
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	done := make(chan bool)
-
-	// Handle file change
-	go func() {
-		for {
-			select {
-			case event := <-watcher.Event:
-
-				if _, err := os.Stat(event.Name); os.IsNotExist(err) {
-					break
-				}
-
-				if strings.Contains(event.Name, ".go") {
-					// Skip Go files
-					break
-				}
-
-				log.Println("Detected change to:", event.Name)
-
-				if strings.Contains(event.Name, "assets") {
-					// Copy asset file
-					err = CopyFile(event.Name, "./generated/"+event.Name)
-					if err != nil {
-						log.Warnln("Couldn't update asset:", err)
-					}
-					break
-				}
-				if strings.Contains(event.Name, "pages") {
-					// Regenerate page
-					k.generatePage(event.Name)
-					break
-				}
-				if strings.Contains(event.Name, "config.toml") {
-					// Parse config again and regenerated all pages
-					err = k.ParseConfig()
-					if err == nil {
-						k.GenerateAllPages()
-					}
-				}
-				if strings.Contains(event.Name, "index.html") {
-					// Parse template again en regenerate all files
-					err = k.ParseTemplates()
-					if err == nil {
-						k.GenerateAllPages()
-					}
-				}
-
-			case err := <-watcher.Error:
-				log.Println("error:", err)
-			}
-		}
-	}()
-
-	err = watcher.Watch("assets")
-	err = watcher.Watch("pages")
-	err = watcher.Watch("./")
-	if err != nil {
-		log.Warnln("Couldn't watch for changes:", err)
-	}
-
-	// Hang so program doesn't exit
-	<-done
-
-	watcher.Close()
+	k.WatchForChanges()
 
 }
---
+++ watcher.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"github.com/howeyc/fsnotify"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"strings"
+)
+
+func (k *Koekr) WatchForChanges() {
+	// Watch for changes
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	done := make(chan bool)
+
+	// Handle file change
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Event:
+
+				if _, err := os.Stat(event.Name); os.IsNotExist(err) {
+					break
+				}
+
+				if strings.Contains(event.Name, ".go") {
+					// Skip Go files
+					break
+				}
+
+				log.Println("Detected change to:", event.Name)
+
+				if strings.Contains(event.Name, "assets") {
+					// Copy asset file
+					err = CopyFile(event.Name, "./generated/"+event.Name)
+					if err != nil {
+						log.Warnln("Couldn't update asset:", err)
+					}
+					break
+				}
+				if strings.Contains(event.Name, "pages") {
+					// Regenerate page
+					k.generatePage(event.Name)
+					break
+				}
+				if strings.Contains(event.Name, "config.toml") {
+					// Parse config again and regenerated all pages
+					err = k.ParseConfig()
+					if err == nil {
+						k.GenerateAllPages()
+					}
+				}
+				if strings.Contains(event.Name, "index.html") {
+					// Parse template again en regenerate all files
+					err = k.ParseTemplates()
+					if err == nil {
+						k.GenerateAllPages()
+					}
+				}
+
+			case err := <-watcher.Error:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Watch("assets")
+	err = watcher.Watch("pages")
+	err = watcher.Watch("./")
+	if err != nil {
+		log.Warnln("Couldn't watch for changes:", err)
+	} else {
+		log.Println("Watching for changes")
+	}
+
+	// Hang so program doesn't exit
+	<-done
+
+	watcher.Close()
+}
